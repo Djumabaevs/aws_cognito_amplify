76d4428d9fb296d2a89671367aae8fa0f14f341a
Djumabaevs <djumabaevb@gmail.com>
2022-11-28 11:12:01 +0600

    sign up worked

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,51 @@ class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('AWS test')),
+      appBar: AppBar(
+        title: const Text('AWS test'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Sign up test'),
+          onPressed: () => signUpUser(),
+        ),
+      ),
     );
+  }
+
+  // Create a boolean for checking the sign up status
+  bool isSignUpComplete = false;
+
+  Future<void> signUpUser() async {
+    try {
+      final userAttributes = <CognitoUserAttributeKey, String>{
+        CognitoUserAttributeKey.email: 'myusername@gmail.com',
+        CognitoUserAttributeKey.phoneNumber: '+15559101234',
+        // additional attributes as needed
+      };
+      final result = await Amplify.Auth.signUp(
+        username: 'myusername@gmail.com',
+        password: 'mysupersecurepassword',
+        options: CognitoSignUpOptions(userAttributes: userAttributes),
+      );
+      setState(() {
+        isSignUpComplete = result.isSignUpComplete;
+      });
+    } on AuthException catch (e) {
+      safePrint(e.message);
+    }
+  }
+
+  Future<void> confirmUser() async {
+    try {
+      final result = await Amplify.Auth.confirmSignUp(
+          username: 'myusername@gmail.com', confirmationCode: '123456');
+
+      setState(() {
+        isSignUpComplete = result.isSignUpComplete;
+      });
+    } on AuthException catch (e) {
+      safePrint(e.message);
+    }
   }
 }
